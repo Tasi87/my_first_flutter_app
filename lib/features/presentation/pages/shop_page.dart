a6738c1135353d075c2e0e270b12e4fc94785965
Tasi <tasi87@gmail.com>
2025-09-19 18:03:50 +0200

    creating webview

--- lib/features/presentation/pages/shop_page.dart
+++ lib/features/presentation/pages/shop_page.dart
@@ -1,1 +1,22 @@
+// import
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
+class ShopPage extends StatelessWidget {
+  const ShopPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(child: _buildWebView());
+  }
+
+  Widget _buildWebView() {
+    // Vytvorenie kontrolera
+    final WebViewController controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted) // povolenie JS
+      ..loadRequest(Uri.parse('https://himdeve.eu')); // načítanie URL
+
+    // Vrátenie WebViewWidget s kontrolerom
+    return WebViewWidget(controller: controller);
+  }
+}
